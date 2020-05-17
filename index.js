8c0daaaf445bba909935bd7b77f539a86faffe3b
Ravi Upadhyay <raviupadhyay.bkn@gmail.com>
2020-05-17 22:41:41 +0200

    chapter-5: Dynamic templating using EJS

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-/* Chapter 4 - Validation of the data in the requests */
-/* Chapter 4-A: Validation of Array and Objects */
+/* Chapter 5 - Dynamic Templating */
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
@@ -12,38 +11,16 @@ app.use('/public', express.static(path.join(__dirname, 'src')));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+// view engine must be set
+app.set('view engine', 'ejs');
+
+// Use render() method instead
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src', 'index.html'));
-});
-
-app.post('/', (req, res) => {
- // Simulate the input received to reduce complexity
- const simulatedReq = {
-   user : {
-     name: 'R M Sharma',
-     address: 'Somewhere in heaven',
-     pincode: '1143ED'
-   },
-   products: ['PL', 'DKVL', 'HD']
- };
-
- const userSchema= Joi.object().keys({
-  name: Joi.string().trim().required(),
-  address: Joi.string().trim().required(),
-  pincode: Joi.string().trim().length(6).required(),
- });
-
- const productsSchema = Joi.array().items(Joi.string());
-
- const requestSchema = Joi.object().keys({
-  user: userSchema,
-  products: productsSchema, 
-});
-
- Joi.validate(simulatedReq, requestSchema, (err, result) => {
-  if(err) console.log(err);
-  else console.log(result);
- });
+  res.render('index', {data: {
+    loggedIn: true,
+    userName: 'R P Sharma',
+    products: ['Personal Loan', 'Life Insurance', 'Vehicle Insurance'],
+  }});
 });
 
 app.listen(3000, () => {
